c1e250e7178865c3b5bae254417136f39ce0bda5
Cauê Santos <34218434+cauebasantos@users.noreply.github.com>
2019-03-28 19:26:37 -0300

    Add files via upload

--- id3/id3.py
+++ id3/id3.py
@@ -3,13 +3,14 @@ import bisect
 import csv
 import random
 from node import Node
+import pandas as pd
 
 dataset = []
 len_dataset = 0
 training_set = []
 test_set = []
 col_labels = []
-
+ 
 def prepare_dataset(filename, split, training_set=[] , test_set=[]):
     # open the file
     with open(filename, 'r') as csvfile:
@@ -61,29 +62,31 @@ def entropy_classifier():
 
     return entropy
 
-def frequency(attr, label):
+def frequency(subset, attr, label):
     class_labels = {}
     n_rows = 0
-    for row in dataset:
+    rows = []
+    for row in subset:
         instance = row[attr]
         if instance == label:
             instance_class = row[-1]
-
+            rows.append(row)
             if instance_class in class_labels:
                 class_labels[instance_class] += 1
             else:
                 class_labels[instance_class] = 1
             n_rows += 1   
     
-    return class_labels, n_rows
+    return rows, class_labels
 
-def calculate_entropy(attr, label):
+def calculate_entropy(subset, attr, label):
     """
     returns the result of p(Classifier|atrr=label)*entropy(Classifier|atrr=label)
     """
     entropy = 0
-    class_labels, n_rows = frequency(attr, label)
-    
+    rows, class_labels = frequency(subset, attr, label)
+    n_rows = len(rows)
+
     for cl in class_labels:
         p = class_labels[cl]/n_rows
         entropy -= p * math.log2(p)
@@ -91,21 +94,21 @@ def calculate_entropy(attr, label):
 #    print(n_rows, len_dataset, entropy)
     return (n_rows/len_dataset)*entropy
 
-def gain(attr):
+def gain(subset, attr):
     """
     returns the gain of the attribute, wich means
     entropy(Classifier) - SUM[p(Classifier|atrr)*entropy(Classifier|attr)]
     """
     gain = entropy_classifier()
     for label in col_labels[attr]:
-        gain -= calculate_entropy(attr, label)
+        gain -= calculate_entropy(subset, attr, label)
 
     return gain
 
-def best_attr(attr_list):
+def best_attr(attr_list, subset):
     gains = []
     for attr in attr_list:
-        g = gain(attr)
+        g = gain(subset, attr)
         bisect.insort(gains, (g, attr))
     #print(gains)
     return gains[-1][1]
@@ -119,27 +122,60 @@ def max_class_label(class_labels):
             max_label = cl
     return max_label
 
-def id3(attr_list, node):
-    node_attr = best_attr(attr_list)
-    attr_list.remove(node_attr)
+def id3(rows_set, node):
+    # if there is no row in the subset
+    # get the attr that best represents the data
+    node_attr = best_attr(attr_list, rows_set)
 
+    # for each one of it's labels
     for label in col_labels[node_attr]:
-        class_labels, n_rows = frequency(node_attr, label)
-        node.children.append(Node(label, node, [], class_labels))
+        # gets the rows the it appears and it's class labels frequency
+        rows, class_labels = frequency(rows_set, node_attr, label)
+        if len(rows) > 0:
+            new_node = Node(node_attr, node, [], rows, class_labels)
+            node.children.append((new_node, label))
     
-    for child in node.children:
+    for pair in node.children:
+        child = pair[0]
         # if label perfectly classified
-        if len(child.class_labels) == 1 or len(attr_list) == 0:
-            leaf_node = Node(max_class_label(class_labels), child, [], class_labels)
-            child.children.append(leaf_node) 
-            return leaf_node
+        if len(child.class_labels) == 1:
+            leaf_label = max_class_label(child.class_labels)
+            leaf_node = Node(leaf_label, child, [], [], child.class_labels)
+            child.children.append((leaf_node, leaf_label)) 
         else:
-            id3(attr_list, child)
+            id3(child.subset, child)
+
+def select_class(case, node):
+    for pair in node.children:
+        child = pair[0]
+        label = pair[1]
+        if len(child.children) == 0:
+            return child.content
+        if case[int(child.content)] == label:
+            return select_class(case, child)
+
+def evaluate_model(root, test_set):
+    real_class = []
+    predict_class = []
     
-attr_list = [1, 2, 3, 4]
-prepare_dataset(r'tenis.txt', 0.70, training_set, test_set)
+    for row in test_set:
+        real = row[-1]
+        predict = select_class(row, root)
+        real_class.append(real)
+        predict_class.append(predict)
+
+    real_class = pd.Series(real_class, name='Actual')
+    predict_class = pd.Series(predict_class, name='Predicted')
+    confusion_matrix = pd.crosstab(real_class, predict_class)
+    return confusion_matrix
+
+
+attr_list = [0, 1, 2, 3, 4, 5]
+prepare_dataset(r'car.data.txt', 0.70, training_set, test_set)
+print('Training set size: ' + repr(len(training_set)))
+print('Test set size: ' + repr(len(test_set)))
 col_labels = preprocess(attr_list)
-print(best_attr(attr_list))
-root = Node(None, None, [], None)
-id3(attr_list, root)
-root.print_tree()
+root = Node(None, None, [], [],  None)
+id3(training_set, root)
+print(evaluate_model(root, test_set))
+#root.print_tree()
--- id3/node.py
+++ id3/node.py
@@ -4,11 +4,12 @@
 class Node:
     """ Node class for ID3 tree
     """
-    def __init__(self, content, parent, children, class_labels):
+    def __init__(self, content, parent, children, subset, class_labels):
         self.content = content
         self.parent = parent
         self.children = children
         self.class_labels = class_labels
+        self.subset = subset
 
     def __repr__(self):
         return f'<Node Object> label: {self.content}, class_labels: {self.class_labels}'
@@ -17,6 +18,6 @@ class Node:
         if not self.children:
             print('Folha', self.content)
         else:
-            print('NF', self.children)
+            print('Pai', self.content)
             for child in self.children:
-                child.print_tree()
+                child[0].print_tree()
